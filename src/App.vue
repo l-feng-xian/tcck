2068f885150cf85cba592667d13913b312e57337
l-feng-xian <2592549445@qq.com>
2022-04-14 17:40:03 +0800

    2
    页面框架

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,72 @@
 <template>
-  <div>xxx</div>
+  <div class="app-box">
+    <div class="app-box-title">图圈查控驾驶舱</div>
+    <div class="app-contnet">
+      <div class="app-content-l">
+        
+        <div class="app-content-l-top">
+          <base-title title="111"></base-title>
+        </div>
+        <div class="app-content-l-center">
+          <base-title title="111"></base-title>
+        </div>
+        <div class="app-content-l-down">
+          <base-title title="111"></base-title>
+        </div>
+      </div>
+      <div class="app-content-c"></div>
+      <div class="app-content-r">
+        <div class="app-content-r-top">
+          <base-title title="111"></base-title>
+        </div>
+        <div class="app-content-r-center">
+          <base-title title="111"></base-title>
+        </div>
+        <div class="app-content-r-down">
+          <base-title title="111"></base-title>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
-// import HelloWorld from './components/HelloWorld.vue'
+import baseTitle from './components/baseTitle.vue';
+import { reactive } from 'vue'
+const leftTopData = reactive({
+  data: { s: 1 }
+})
+console.log(leftTopData, 'leftTopData');
 </script>
 
 
 <style>
-@import './assets/css/reset.css'
+@import './assets/css/reset.css';
+
+.app-box {
+  height: 100vh;
+  width: 100vw;
+}
+
+.app-box-title {
+  height: 50px;
+  line-height: 50px;
+  text-align: center;
+}
+
+.app-contnet {
+  height: calc(100% - 50px);
+  width: 100%;
+  display: flex;
+}
+
+.app-content-l,
+.app-content-r {
+  flex: 1;
+  background-color: pink;
+}
+
+.app-content-c {
+  flex: 2;
+}
 </style>
---
+++ src/components/baseTitle.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="base-title">
+      <span></span>
+      <p>{{props.title}}</p>
+      <slot></slot>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    title: String
+})
+</script>
+
+<style scoped>
+.base-title {
+  width: 100%;
+  height: 40px;
+  display: flex;
+}
+</style>
